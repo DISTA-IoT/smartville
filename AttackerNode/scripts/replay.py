7da09b59a80cd6857b6b9f88ac1b2b9bfabacd55
Qwerty Jacob <jesusfcevallos@gmail.com>
2024-01-31 14:48:05 +0100

    REPEATING ATTACKS...

--- AttackerNode/scripts/replay.py
+++ AttackerNode/scripts/replay.py
@@ -2,6 +2,8 @@ import os
 from scapy.all import *
 import netifaces as ni
 from scapy.all import IP
+import threading
+import argparse
 
 
 SOURCE_IP = None
@@ -9,6 +11,7 @@ SOURCE_MAC = None
 TARGET_IP = None
 IFACE_NAME = 'eth0'
 PATTERN_TO_REPLAY = None
+REPEAT_PATTERN_SECS = None
 
 def get_source_ip_address(interface=IFACE_NAME):
     try:
@@ -28,23 +31,18 @@ def get_source_mac(interface=IFACE_NAME):
 
 def modify_and_send(packet):
 
+    # focus on level 3
     ip_packet = packet[IP]
+
     # Modify source IP
     ip_packet.src = SOURCE_IP
+
     # Modify destination IP
     ip_packet.dst = TARGET_IP
+
     # Send the modified packet
-
-    
-
-    # Send the packet
-    # eth_frame = Ether(src=SOURCE_MAC, dst="ff:ff:ff:ff:ff:ff")
-    # Combine Ethernet frame and IP packet
-    # new_packet = eth_frame / ip_packet
-    # sendp(new_packet, iface=IFACE_NAME)  # Specify the interface to send the packet from (e.g., eth0)
-
     send(ip_packet, iface=IFACE_NAME)
-    print('hello')
+   
 
 
 def resend_pcap_with_modification():
@@ -52,7 +50,7 @@ def resend_pcap_with_modification():
     for filename in os.listdir(PATTERN_TO_REPLAY):
         if filename.endswith(".pcap"):
             pcap_file = os.path.join(PATTERN_TO_REPLAY, filename)
-            print("Processing file:", pcap_file)
+            # print("Processing file:", pcap_file)
 
             # Read the PCAP file
             packets = rdpcap(pcap_file)
@@ -64,24 +62,48 @@ def resend_pcap_with_modification():
                     modify_and_send(packet)
 
 
+
+def repeat_function():
+    
+    resend_pcap_with_modification()  # Execute the function immediately
+    
+    if REPEAT_PATTERN_SECS is not None:
+        print(f'Will repeat pattern in {REPEAT_PATTERN_SECS} seconds')
+        threading.Timer(
+            REPEAT_PATTERN_SECS, 
+            repeat_function).start()
+    
+
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print("Usage: python3 replay.py PATTERN_TO_REPLAY TARGET_IP")
         sys.exit(1)
+
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Replay script with optional repeat argument")
+    parser.add_argument("PATTERN_TO_REPLAY", help="Pattern to replay directory")
+    parser.add_argument("TARGET_IP", help="Target IP address")
+    parser.add_argument("--repeat", type=int, default=None, help="Number of seconds before repeating the pattern (default: Don't repeat)")
+
+    args = parser.parse_args()
 
     # Your new source IP
     SOURCE_IP = get_source_ip_address()
     SOURCE_MAC = get_source_mac()
 
-    PATTERN_TO_REPLAY = sys.argv[1]  # Get the PATTERN_TO_REPLAY DIRECTORY from the command line arguments
-    TARGET_IP = sys.argv[2]  # Get the TARGET_IP from the command line arguments
+    # Get the values from command line arguments
+    PATTERN_TO_REPLAY = args.PATTERN_TO_REPLAY
+    TARGET_IP = args.TARGET_IP
+    REPEAT_PATTERN_SECS = args.repeat
+
 
     print(f'Source IP {SOURCE_IP}')
     print(f'Source MAC {SOURCE_MAC}')
     print(f'Target IP {TARGET_IP}')
     print(f'Pattern to replay: {PATTERN_TO_REPLAY}')
+    print(f'Interval between replays: {REPEAT_PATTERN_SECS}')
 
 
     # Resend packets with modified IPs
-    resend_pcap_with_modification()
+    repeat_function()
--- VictimNode/scripts/replay.py
+++ VictimNode/scripts/replay.py
@@ -2,6 +2,8 @@ import os
 from scapy.all import *
 import netifaces as ni
 from scapy.all import IP
+import threading
+import argparse
 
 
 SOURCE_IP = None
@@ -9,6 +11,7 @@ SOURCE_MAC = None
 TARGET_IP = None
 IFACE_NAME = 'eth0'
 PATTERN_TO_REPLAY = None
+REPEAT_PATTERN_SECS = None
 
 def get_source_ip_address(interface=IFACE_NAME):
     try:
@@ -47,7 +50,7 @@ def resend_pcap_with_modification():
     for filename in os.listdir(PATTERN_TO_REPLAY):
         if filename.endswith(".pcap"):
             pcap_file = os.path.join(PATTERN_TO_REPLAY, filename)
-            print("Processing file:", pcap_file)
+            # print("Processing file:", pcap_file)
 
             # Read the PCAP file
             packets = rdpcap(pcap_file)
@@ -59,24 +62,48 @@ def resend_pcap_with_modification():
                     modify_and_send(packet)
 
 
+
+def repeat_function():
+    
+    resend_pcap_with_modification()  # Execute the function immediately
+    
+    if REPEAT_PATTERN_SECS is not None:
+        print(f'Will repeat pattern in {REPEAT_PATTERN_SECS} seconds')
+        threading.Timer(
+            REPEAT_PATTERN_SECS, 
+            repeat_function).start()
+    
+
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print("Usage: python3 replay.py PATTERN_TO_REPLAY TARGET_IP")
         sys.exit(1)
+
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Replay script with optional repeat argument")
+    parser.add_argument("PATTERN_TO_REPLAY", help="Pattern to replay directory")
+    parser.add_argument("TARGET_IP", help="Target IP address")
+    parser.add_argument("--repeat", type=int, default=None, help="Number of seconds before repeating the pattern (default: Don't repeat)")
+
+    args = parser.parse_args()
 
     # Your new source IP
     SOURCE_IP = get_source_ip_address()
     SOURCE_MAC = get_source_mac()
 
-    PATTERN_TO_REPLAY = sys.argv[1]  # Get the PATTERN_TO_REPLAY DIRECTORY from the command line arguments
-    TARGET_IP = sys.argv[2]  # Get the TARGET_IP from the command line arguments
+    # Get the values from command line arguments
+    PATTERN_TO_REPLAY = args.PATTERN_TO_REPLAY
+    TARGET_IP = args.TARGET_IP
+    REPEAT_PATTERN_SECS = args.repeat
+
 
     print(f'Source IP {SOURCE_IP}')
     print(f'Source MAC {SOURCE_MAC}')
     print(f'Target IP {TARGET_IP}')
     print(f'Pattern to replay: {PATTERN_TO_REPLAY}')
+    print(f'Interval between replays: {REPEAT_PATTERN_SECS}')
 
 
     # Resend packets with modified IPs
-    resend_pcap_with_modification()
+    repeat_function()
