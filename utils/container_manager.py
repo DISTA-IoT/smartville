f40ad0c2edd906649223765a903426a564c1008a
QwertyJacob <jesusfcevallos@gmail.com>
2024-03-18 16:12:55 +0100

    container manager refactor

--- utils/container_manager.py
+++ utils/container_manager.py
@@ -19,23 +19,21 @@ import docker
 import threading
 import time
 import subprocess
+import argparse
 
 TERMINAL_ISSUER_PATH = './terminal_issuer.sh'  # MAKE IT EXECUTABLE WITH chmod +x terminal_issuer.sh
-
 BROWSER_PATH = '/usr/bin/brave-browser'  # Change to your commodity browser
 CONTROLLER_IMG_NAME = 'pox-controller:latest'
 ATTACKER_IMG_NAME = 'attacker:latest'
 VICTIM_IMG_NAME = 'victim:latest'
 
-CURRICULUM=1
-print(f'CURRICULUM is {CURRICULUM}')
-KNWON_TRAFFIC_NODES = []
+CURRICULUM=None # For training and labelling purposes. Must be set according to the labelling in the controller. 
+KNOWN_TRAFFIC_NODES = []
 TRAINING_ZDA_NODES = []
 TEST_ZDA_NODES = []  
 
 process_ids = {}
 containers_dict = {}
-
 
 start_zookeeper_command = "zookeeper-server-start.sh pox/smartController/zookeeper.properties"
 start_kafka_command = "kafka-server-start.sh pox/smartController/kafka_server.properties"
@@ -94,6 +92,7 @@ def launch_grafana_detached(controller_container):
     print('Linking Grafana to Prometheus...')
     print(run_command_in_container(controller_container, "python3 pox/smartController/link_grafana_to_prometheus.py"))
     time.sleep(1)
+
 
 def launch_zookeeper(controller_container):
     output_thread = threading.Thread(
@@ -161,7 +160,6 @@ def launch_controller_processes(controller_container):
     launch_brower_consoles(controller_container)
 
 
-
 def launch_metrics():
     for container_name, container_obj in containers_dict.items():
         if container_name.startswith('victim'):
@@ -185,28 +183,27 @@ def kill_attacks():
         print(f"Killed process {pid} in {container_info['Config']['Hostname']}")
 
 
-def switch_case(argument):
+def pattern_replay_commands(argument):
+    """
+    Modify this function to adjust it to your topolgy and desired pattern replay dynamics.
+    """
     switch_cases = {
         # Known Bening:
         'victim-0': 'python3 replay.py doorlock 192.168.1.6 --repeat 10',
         'victim-1': 'python3 replay.py echo 192.168.1.5 --repeat 10',
         'victim-2': 'python3 replay.py hue 192.168.1.3 --repeat 10',
         'victim-3': 'python3 replay.py doorlock 192.168.1.4 --repeat 10',
-        # Known Attacks:
         'attacker-4': 'python3 replay.py cc_heartbeat 192.168.1.3 --repeat 10',
         'attacker-5': 'python3 replay.py generic_ddos  192.168.1.4 --repeat 10',
         'attacker-6': 'python3 replay.py h_scan 192.168.1.5 --repeat 10',
-        # Training ZdAs:
         'attacker-7': 'python3 replay.py hakai 192.168.1.6 --repeat 10',
         'attacker-8': 'python3 replay.py torii 192.168.1.4 --repeat 10',
         'attacker-9': 'python3 replay.py mirai 192.168.1.5 --repeat 10',
         'attacker-10': 'python3 replay.py gafgyt 192.168.1.3 --repeat 10',
-        # Test ZdAs:
         'attacker-11': 'python3 replay.py hajime 192.168.1.6 --repeat 10',
         'attacker-12': 'python3 replay.py okiru 192.168.1.5 --repeat 10',
         'attacker-13': 'python3 replay.py muhstik 192.168.1.3 --repeat 10',
-
-        'default': 'echo hello'
+        'default': None
     }
     return switch_cases.get(argument, switch_cases['default'])
 
@@ -214,11 +211,11 @@ def switch_case(argument):
 def send_known_traffic():
     args = []
 
-    for container_img_name in KNWON_TRAFFIC_NODES:
+    for container_img_name in KNOWN_TRAFFIC_NODES:
         container = containers_dict[container_img_name]
 
-        command_to_run = switch_case(container_img_name)
-        if command_to_run != 'echo hello':
+        command_to_run = pattern_replay_commands(container_img_name)
+        if command_to_run:
             args.append(f"{container.id}:{container_img_name}:{command_to_run}")
 
     # Build the command to execute your Bash script with its arguments
@@ -241,7 +238,7 @@ def send_training_zdas():
     for container_img_name in TRAINING_ZDA_NODES:
 
         curr_container = containers_dict[container_img_name]
-        command_to_run = switch_case(container_img_name)
+        command_to_run = pattern_replay_commands(container_img_name)
         args.append(f"{curr_container.id}:{container_img_name} (Training ZdA):{command_to_run}")
     
     # Build the command to execute your Bash script with its arguments
@@ -264,7 +261,7 @@ def send_test_zdas():
     for container_img_name in TEST_ZDA_NODES:
 
         curr_container = containers_dict[container_img_name]
-        command_to_run = switch_case(container_img_name)
+        command_to_run = pattern_replay_commands(container_img_name)
         args.append(f"{curr_container.id}:{container_img_name} (Test ZdA):{command_to_run}")
     
     # Build the command to execute your Bash script with its arguments
@@ -293,7 +290,7 @@ def launch_traffic():
         # print(container_img_name)
         # print("Container IP:", container_ip)
         # Get the proper command
-        command_to_run = switch_case(container_img_name)
+        command_to_run = pattern_replay_commands(container_img_name)
         if command_to_run != 'echo hello':
             args.append(f"{container.id}:{container_info_str}:{command_to_run}")
 
@@ -310,36 +307,26 @@ def launch_traffic():
         print("Error:", e)
 
 
-def stop_nodes():
-    """
-    Have no time? Do it by hand on GNS3, it is much faster...
-    """
-    for container_img_name, container in containers_dict.items():
-        if 'attacker' in container_img_name or 'victim' in container_img_name:
-            print(f'gracefully stoping {container_img_name}')
-            container.stop()
-
-
-def start_nodes():
-
-    for container_img_name, container in containers_dict.items():
-        if 'attacker' in container_img_name or 'victim' in container_img_name:
-            print(f'starting {container_img_name}')
-            container.start()
-
-
-
 def send_all_traffic():
     send_known_traffic()
     send_training_zdas()
     send_test_zdas()
     
 
-def restart_nodes():
-    stop_nodes()
-    start_nodes()
-
 if __name__ == "__main__":
+
+
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Container Manager script")
+    parser.add_argument(
+        "--curriculum", 
+        help="The curriculum to divide known, train unknown and test unkown patterns for ZdA Attack detection  (Default is 1)", 
+        default=1)
+    
+    args = parser.parse_args()
+
+    CURRICULUM = args.curriculum
+
 
     # Connect to the Docker daemon
     client = docker.from_env()
@@ -358,7 +345,7 @@ if __name__ == "__main__":
     
     if CURRICULUM == 0:
 
-        KNWON_TRAFFIC_NODES = [
+        KNOWN_TRAFFIC_NODES = [
         'victim-0',
         'victim-1',
         'victim-2',
@@ -380,7 +367,7 @@ if __name__ == "__main__":
 
     elif CURRICULUM == 1:
 
-        KNWON_TRAFFIC_NODES = [
+        KNOWN_TRAFFIC_NODES = [
         'victim-0',
         'victim-1',
         'victim-2',
@@ -401,11 +388,9 @@ if __name__ == "__main__":
         'attacker-6',
         'attacker-13']  
 
-
-
     elif CURRICULUM == 2:
 
-        KNWON_TRAFFIC_NODES = [
+        KNOWN_TRAFFIC_NODES = [
         'victim-0',
         'victim-1',
         'victim-2',
@@ -426,19 +411,19 @@ if __name__ == "__main__":
         'attacker-8']  
 
 
-    user_input = input("Press '0' to send all traffic, \n" +\
-                       "'r' to restart nodes, \n" +\
-                       "'1' to send known traffic, \n" +\
-                       "'2' to send training zdas \n" +\
-                       "'3' to send test zdas \n" +\
-                        "'4' to send metrics, \n" +\
-                        "'5' to launch controller services, \n"+\
-                        "or 'q' to quit: ")
+    user_input = input("SmartVille Container Maganer \n" +\
+                       "Plase input a character and type enter. \n" +\
+                       "'c' to launch controller services, \n"+\
+                       "'0' to send all patterns from nodes, \n" +\
+                       f"'1' to send only known traffic according to (curriculum {CURRICULUM}), \n" +\
+                       f"'2' to send only training zdas (curriculum {CURRICULUM})\n" +\
+                       f"'3' to send only test zdas (curriculum {CURRICULUM})\n" +\
+                       "'4' to send node features from all nodes, \n" +\
+                       "'s' to stop network nodes (faster from GNS3), \n" +\
+                       "or 'q' to quit: ")
     
     if user_input == '0':
         send_all_traffic()
-    elif user_input == 'r':
-        restart_nodes()
     elif user_input == '1':
         send_known_traffic()
     elif user_input == '2':
@@ -447,7 +432,7 @@ if __name__ == "__main__":
         send_test_zdas()
     elif user_input == '4':
         launch_metrics()
-    elif user_input == '5':
+    elif user_input == 'c':
         launch_controller_processes(containers_dict['pox-controller-1'])
     elif user_input == 'pro':
         print(launch_prometheus_detached(containers_dict['pox-controller-1']))
@@ -461,7 +446,5 @@ if __name__ == "__main__":
         print(launch_kafka_detached(containers_dict['pox-controller-1']))
     elif user_input == 'dkl':
         print(delete_kafka_logs(containers_dict['pox-controller-1']))
-    elif user_input == 'stop':
-        stop_nodes()
-    elif user_input == 'start':
-        start_nodes()
+    elif user_input == 'q':
+        exit
