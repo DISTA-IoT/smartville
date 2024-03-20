34b3a4403239d0da3f7c7c9fc68859109e4ac2ff
QwertyJacob <jesusfcevallos@gmail.com>
2024-03-20 13:05:23 +0100

    refac

--- poxController/smartController/set_prometheus.py
+++ poxController/smartController/set_prometheus.py
@@ -32,27 +32,6 @@ def get_source_ip_address():
         return "Interface not found"
 
 
-def replace_string_in_file(file_name, old_string, new_string):
-    try:
-        # Read the content of the file
-        with open(file_name, 'r') as file:
-            file_content = file.read()
-
-        # Replace the old string with the new string
-        modified_content = file_content.replace(old_string, new_string)
-
-        # Write the modified content back to the file
-        with open(file_name, 'w') as file:
-            file.write(modified_content)
-
-        print(f"String '{old_string}' replaced with '{new_string}' in {file_name}")
-    except FileNotFoundError:
-        print(f"Error: File '{file_name}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-
 def generate_prometheus_config(ip):
     config = f"""\
 global:
@@ -87,10 +66,7 @@ scrape_configs:
         f.write(config)
 
 
-
 if __name__ == "__main__":
-    # file_name = "pox/smartController/prometheus.yml"
-    # old_string = "localhost"
     dynamic_IP = get_source_ip_address()
-    # replace_string_in_file(file_name, old_string, dynamic_IP)
+    print(f'Generating Prometheus config file with ip {dynamic_IP}')
     generate_prometheus_config(dynamic_IP)
